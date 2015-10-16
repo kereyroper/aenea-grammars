b505eb9584a426792866c7632aa7a1cbf35aeb5a
Kerey Roper <kroper@hearsaycorp.com>
2015-10-16 13:11:03 -0700

    Add grammar for general OS X shortcuts

---
+++ _darwin_general.py
@@ -0,0 +1,33 @@
+# Author: Kerey Roper
+#
+# Licensed under LGPL
+
+import aenea
+import aenea.configuration
+from aenea import IntegerRef
+from aenea.lax import Key
+import dragonfly
+
+context = aenea.ProxyPlatformContext('darwin')
+grammar = dragonfly.Grammar('darwin_general', context=context)
+
+
+class Mapping(dragonfly.MappingRule):
+    mapping = {
+        'select': Key("win:up"),
+        'task': Key("win:down") + Key("tab"),
+    }
+    extras = [
+        IntegerRef('n', 1, 99),
+    ]
+
+
+grammar.add_rule(Mapping())
+grammar.load()
+
+
+def unload():
+    global grammar
+    if grammar:
+        grammar.unload()
+    grammar = None
