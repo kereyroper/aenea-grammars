e6b562bdfcfab2fd676d5ab32002e216ed9b31dc
Kerey Roper <kroper@hearsaycorp.com>
2015-10-28 22:02:22 -0700

    Add support for committing changed files in single command

--- _git.py
+++ _git.py
@@ -21,6 +21,7 @@ git_mapping = aenea.configuration.make_grammar_commands('git', {
 
     'git amend': Text("git commit --amend") + Key("enter"),
     'git commit': Text("git commit") + Key("enter"),
+    'git commit (changes|changed)': Text("git commit -a") + Key("enter"),
     'git pull': Text("git pull") + Key("enter"),
     'git branches': Text("git branch -l") + Key("enter"),
     'git status': Text("git status") + Key("enter"),
