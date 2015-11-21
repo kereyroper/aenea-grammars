105ac633687980a0d4e105b4491ae8f319de0404
Kerey Roper <kroper@hearsaycorp.com>
2015-11-20 16:00:39 -0800

    Add command for amending commits

--- _git.py
+++ _git.py
@@ -21,6 +21,8 @@ git_mapping = aenea.configuration.make_grammar_commands('git', {
 
     'git amend': Text("git commit --amend") + Key("enter"),
     'git commit': Text("git commit") + Key("enter"),
+    'git commit amend': Text("git commit --amend") + Key("enter"),
+    'git commit amend (changes|changed)': Text("git commit --amend -a") + Key("enter"),
     'git commit (changes|changed)': Text("git commit -a") + Key("enter"),
     'git fetch': Text("git fetch upstream") + Key("enter"),
     'git fetch all': Text("git fetch --all") + Key("enter"),
