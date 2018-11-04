9a5ef99742e7bd05d3af4ac24c7264478430602f
Kerey Roper <kereyroper@users.noreply.github.com>
2018-11-04 00:11:45 -0700

    remove unused programs

--- _jargon.py
+++ _jargon.py
@@ -22,6 +22,7 @@ context = aenea.ProxyPlatformContext('darwin')
 grammar = dragonfly.Grammar('jargon', context=context)
 
 basics_mapping = aenea.configuration.make_grammar_commands('jargon', {
+    'H (base|space)': Text('HBase'),
     })
 
 
