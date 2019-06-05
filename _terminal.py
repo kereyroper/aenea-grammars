795f78925cc7d24f5428c8aa5e6269d2824a9b4f
Kerey Roper <kereyroper@users.noreply.github.com>
2019-06-05 14:40:29 -0700

    alias for ssh

--- _terminal.py
+++ _terminal.py
@@ -57,7 +57,7 @@ terminal_mapping = aenea.configuration.make_grammar_commands('terminal', {
     "process re nice low": Text("renice -n 10 "),
     "process re nice hi": Text("renice -n -10 "),
 
-    "shell connect": Text("ssh "),
+    "shell [connect|into]": Text("ssh "),
     "shell disconnect": Key("enter") + Key("tilde") + Key("dot"),
 
     "some": Text("paste -sd+ - | bc"),
